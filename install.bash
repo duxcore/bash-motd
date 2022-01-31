1300f19bc81bd42f338b50bbb3fcc8960a7180d5
HoloPanio <30759238+HoloPanio@users.noreply.github.com>
2022-01-31 17:28:58 -0600

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo rm -rf /etc/update-motd.d/*
-sudo curl https://raw.githubusercontent.com/duxcore/bash-motd/dev/00-header -O /etc/update-motd.d/00-header
+sudo curl https://raw.githubusercontent.com/duxcore/bash-motd/dev/00-header --output /etc/update-motd.d/00-header
